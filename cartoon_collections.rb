5f652d8aa4052fce3850a9e8ab9b75ccb733a4bf
Adit P Patel <app2116@gmail.com>
2020-01-14 21:53:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,10 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{|val, index| puts "#{index + 1}. #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  dwarves.each_with_index{|val, index| puts "#{index + 1}. #{val}" }end
+def summon_captain_planet(planeteers) # code an argument here
+  result = dwarves.collect{|n| n.capitalize}
+  return result
+  end
 
 def long_planeteer_calls# code an argument here
   # Your code here
